fd724664637adba00cd465881a9695f0980e894b
Nico-Itorma <nicoitorma38@gmail.com>
2022-09-11 12:27:09 +0800

    StudentListPage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:barcode_attendance/Views/studentPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:barcode_attendance/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePage());
+    await tester.pumpWidget(const StudentPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
