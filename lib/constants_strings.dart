9779bb302a383b1ab326380466e6186da045b050
Nico Itorma <nicoitorma38@gmail.com>
2022-09-13 17:53:35 +0800

    new feature

--- lib/constants_strings.dart
+++ lib/constants_strings.dart
@@ -1,2 +1,6 @@
 const String NO_ATTENDANCE_AVAILABLE = "No attendance available";
 const String ADD_STUDENT_TOOLTIP = "Add Attendance";
+const String INVALID_EMAIL = "Please enter a valid email address";
+const String INVALID_PASSWORD = "Please enter your password";
+const String EMAIL_ADDRESS = "Email address";
+const String PASSWORD = "Password";
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:barcode_attendance/Views/LogIn.dart';
 import 'package:barcode_attendance/Views/AttendancePage.dart';
 import 'package:flutter/material.dart';
 
 import 'Models/Database/AttendanceDB.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   // For attendanceDB or normal user
+//   final database =
+//       await $FloorAttendanceDatabase.databaseBuilder('AttendanceDB.db').build();
+//   final attendanceDao = database.attendanceDao;
+//
+//   //For studentDB or admin access
+//   //TODO: add studentDB, and check if user is admin or not
+//
+//   //runApp(AttendancePage(attendanceDao: attendanceDao));
+//
+// }
 
-  // For attendanceDB or normal user
-  final database =
-      await $FloorAttendanceDatabase.databaseBuilder('AttendanceDB.db').build();
-  final attendanceDao = database.attendanceDao;
-
-  //For studentDB or admin access
-  //TODO: add studentDB and check if user is admin or not
-
-  runApp(AttendancePage(attendanceDao: attendanceDao));
+void main() {
+  runApp(const LoginPage());
 }
